e827bd26b98658a91749987ee155c0381ffcd994
Ruben Post <Ruben-post@hotmail.nl>
2021-04-04 16:44:46 +0000

    Encoder update

--- preprocessing/encoding.py
+++ preprocessing/encoding.py
@@ -4,18 +4,28 @@ import pandas as pd
 
 class encoder:
 
-    def categorical_encoder(preprocessed):
+    def categorical_encoder(self):
         start = time.time()
-        print("Starting categorical encoding..")        
-        dummies = pd.get_dummies(preprocessed.object_cols.drop(['EventID', 'case:concept:name', 'OfferID'], axis=1))
+        print("Starting categorical encoding..")      
+        encoded_categorical = self.object_cols.groupby(['case:concept:name']).progress_apply(encoder.categorical_encoder_apply, preprocessed = self)
         end = time.time()
         print("Categorical encoding took", end - start, "seconds.")
-        return pd.concat([dummies, preprocessed.num_cols, pd.DataFrame(preprocessed.case_id_col)], axis=1)
+        return encoded_categorical
 
-    def numeric_encoder(preprocessed, numeric_encoding):
+    def categorical_encoder_apply(self, preprocessed):
+        encoded_categorical = pd.DataFrame()
+        for column in preprocessed.dynamic_cat_cols.columns:#drop(['EventID'], axis=1).columns:
+            dynamic_object = self[column].value_counts().reset_index()
+            dynamic_object = dynamic_object.pivot_table(columns='index', values=column).reset_index()
+            dynamic_object.drop('index', axis=1, inplace=True)
+            encoded_categorical = pd.concat([encoded_categorical, dynamic_object], axis=1)
+        encoded_categorical = encoded_categorical.reset_index()
+        return encoded_categorical
+
+    def numeric_encoder(self, numeric_encoding):
         start = time.time()
         print("Starting numeric encoding..")
-        preprocessed_data = preprocessed.groupby(['case:concept:name'], as_index=False).agg(numeric_encoding)
+        preprocessed_data = self.data.groupby(['case:concept:name'], as_index=False).agg(numeric_encoding)
         preprocessed_data = preprocessed_data.drop(['case:concept:name'], axis=1)
         end = time.time()
         print("Numeric encoding took", end - start, "seconds.")
